23f46a173368c43de2e006d33ec17315b3754f78
jakdot <j.dotlacil@gmail.com>
2018-04-12 12:31:13 +0200

    Class 2 homework cleaned

--- weekly tasks/week 2/homework/code/start_code.py
+++ weekly tasks/week 2/homework/code/start_code.py
@@ -6,7 +6,7 @@ import random #we import the random module, to be able to randomly select elemen
 from collections import Counter #this can be useful if you are going to use Counter as a container for word frequencies
 
 #The code below asks user for path to text file; the answer of the user is stored in the variable textFile
-textFile = str(raw_input('Please enter the path to the text file you want to read: '))
+textFile = str(input('Please enter the path to the text file you want to read: '))
 
 #the code below reads in the textfile specified and loops through the file line by line
 def collect_frequencies(nameoffile):
@@ -16,14 +16,17 @@ def collect_frequencies(nameoffile):
     with open(textFile) as text:
         for line in text:
             pass
-            #your first task is to substitute pass with a real code. You have to split line into words using white spaces, lower its characters and store the results. For this task, check the documentation about methods of strings, here:
+            #your first task is to substitute pass with a real code. You have to split line into words using white spaces, lower its characters. Then, it has to count all the words just found, creating one dictionary with frequencies stored. The dictionary is then outputed. For example, if the text was this:
+            #Mary sleeps.
+            #John sleeps too.
+            #The dictionary would be: {'mary': 1, 'john': 1, 'sleeps': 2, 'too': 1}. For this task, check the documentation about methods of strings, here:
 #https://docs.python.org/3.5/library/stdtypes.html
 
 def find_frequent_words(word_frequencies, amount=50):
     """
     Return two lists. The first list is the list of amount-many most frequent words, ordered by frequency (starting with the most frequent). The second list is the list of corresponding frequencies.
 
-    The first argument, word_frequencies, is the dictionary or the counter of word frequencies, created in the function collect_frequencies.
+    The first argument of this function, word_frequencies, is the dictionary or the counter of word frequencies, created in the function collect_frequencies.
     """
     pass
     #Delete pass and in its place, specify the function; the function should return two lists - one with words, the other one with frequencies; note that you can return two lists by simply writing return list1, list2
@@ -31,9 +34,11 @@ def find_frequent_words(word_frequencies, amount=50):
 def find_word(word_frequencies, word=None):
     """
     Returns two lists, each list consisting of just one element. The first list has the string identical to the specified word, the second list is its frequency. If no word is specified, a random word is picked.
+
+    The first argument of this function, word_frequencies, is the dictionary or the counter of word frequencies, created in the function collect_frequencies.
     """
     pass
-    #Delete pass and in its place; specify the function
+    #Delete pass and in its place, specify the function
 
 def print_lists(list1, list2):
     """
